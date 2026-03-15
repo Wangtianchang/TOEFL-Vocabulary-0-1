b1e636fe717cf96de85d37cdb47f3a93665a10da
Wangtianchang <tim-tianchang@hotmail.com>
2026-03-16 01:03:36 +0800

    Add initial word list to data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const wordList = [
+    {w: "abandon", p: "/əˈbændən/", d: "v. 抛弃，舍弃，放弃", e: "The captain abandoned the ship."},
+    {w: "ability", p: "/əˈbɪlɪtɪ/", d: "n. 能力；才能", e: "He has the ability to succeed."},
+    {w: "abnormal", p: "/æbˈnɔːm(ə)l/", d: "a. 反常的，变态的", e: "It's abnormal to have such hot weather in winter."},
+    {w: "abolish", p: "/əˈbɔlɪʃ/", d: "v. 废除，废止", e: "The system was abolished in 1990."},
+    {w: "abrupt", p: "/əˈbrʌpt/", d: "a. 突然的，意外的", e: "The bus came to an abrupt stop."},
+    {w: "absorb", p: "/əbˈsɔːb/", d: "v. 吸收，使全神贯注", e: "Plants absorb carbon dioxide."},
+    {w: "abstract", p: "/ˈæbstrækt/", d: "a./n. 抽象的", e: "Abstract art can be difficult to understand."},
+    {w: "academic", p: "/ˌækəˈdemɪk/", d: "a. 学术的", e: "She has high academic standards."},
+    {w: "accelerate", p: "/əkˈseləreɪt/", d: "v. 加速", e: "The car accelerated as it left the town."}
+    // ... 这里可以持续添加你词表里的其他单词
+];
