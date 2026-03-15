4e3915422541e44f884ee78f3e7789cfa0d9b936
Wangtianchang <tim-tianchang@hotmail.com>
2026-03-16 01:15:10 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,21 @@
 const wordList = [
+    {w: "a", p: "/ə,eɪ(ən)/", d: "art. 一（个、件……）", e: "This is a book."},
     {w: "abandon", p: "/əˈbændən/", d: "v. 抛弃，舍弃，放弃", e: "The captain abandoned the ship."},
     {w: "ability", p: "/əˈbɪlɪtɪ/", d: "n. 能力；才能", e: "He has the ability to succeed."},
-    {w: "abnormal", p: "/æbˈnɔːm(ə)l/", d: "a. 反常的，变态的", e: "It's abnormal to have such hot weather in winter."},
-    {w: "abolish", p: "/əˈbɔlɪʃ/", d: "v. 废除，废止", e: "The system was abolished in 1990."},
-    {w: "abrupt", p: "/əˈbrʌpt/", d: "a. 突然的，意外的", e: "The bus came to an abrupt stop."},
-    {w: "absorb", p: "/əbˈsɔːb/", d: "v. 吸收，使全神贯注", e: "Plants absorb carbon dioxide."},
-    {w: "abstract", p: "/ˈæbstrækt/", d: "a./n. 抽象的", e: "Abstract art can be difficult to understand."},
-    {w: "academic", p: "/ˌækəˈdemɪk/", d: "a. 学术的", e: "She has high academic standards."},
-    {w: "accelerate", p: "/əkˈseləreɪt/", d: "v. 加速", e: "The car accelerated as it left the town."}
-    // ... 这里可以持续添加你词表里的其他单词
+    {w: "able", p: "/ˈeɪb(ə)l/", d: "a. 能够；有能力的", e: "She is able to speak three languages."},
+    {w: "abnormal", p: "/æbˈnɔːm(ə)l/", d: "a. 反常的，变态的", e: "The test results are abnormal."},
+    {w: "aboard", p: "/əˈbɔːd/", d: "prep. 上（船，飞机等）", e: "Welcome aboard!"},
+    {w: "abolish", p: "/əˈbɔlɪʃ/", d: "v. 废除，废止", e: "Slavery was abolished long ago."},
+    {w: "abortion", p: "/əˈbɔːʃ(ə)n/", d: "v. 人工流产，堕胎", e: "The law on abortion is strict."},
+    {w: "about", p: "/əˈbaʊt/", d: "ad. 大约；到处 prep. 关于", e: "What are you talking about?"},
+    {w: "above", p: "/əˈbʌv/", d: "prep. 在……上面", e: "The plane is flying above the clouds."},
+    {w: "abroad", p: "/əˈbrɔːd/", d: "ad. 到国外", e: "She often goes abroad."},
+    {w: "abrupt", p: "/əˈbrʌpt/", d: "a. 突然的，意外的", e: "The meeting came to an abrupt end."},
+    {w: "absence", p: "/ˈæbsəns/", d: "n. 不在，缺席", e: "In the absence of proof, he is innocent."},
+    {w: "absent", p: "/ˈæbsənt/", d: "a. 缺席的", e: "He was absent from school today."},
+    {w: "absolute", p: "/ˈæbsəluːt/", d: "a. 绝对的，完全的", e: "This is the absolute truth."},
+    {w: "absorb", p: "/əbˈsɔːb/", d: "v. 吸收，使全神贯注", e: "Sponges absorb water."},
+    {w: "abstract", p: "/ˈæbstrækt/", d: "a./n. 抽象的", e: "Abstract concepts are hard to grasp."},
+    {w: "absurd", p: "/əbˈsɜːd/", d: "a. 荒谬的", e: "That idea is totally absurd."}
+    // (后面我会继续按顺序给你生成后续字母的数据包)
 ];
